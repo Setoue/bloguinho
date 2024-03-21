c24b3f2b2be2adc96a617095b2d3fd3bc7852e09
Setoue <proshigeru@gmail.com>
2024-03-21 20:15:01 -0300

    feat: add footer and navbar components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <BrowserRouter>
         <div className="container">
           <Routes>
@@ -14,6 +17,7 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
